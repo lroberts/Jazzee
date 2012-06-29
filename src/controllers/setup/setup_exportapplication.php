c347aba9b71edbbc5f2e2149c5a1fbe24f620f2c
Jonathan Johnson <jon.johnson@ucsf.edu>
2012-06-28 17:50:47 -0700

    Backport [f5c10a4692a14e7aec407771f9a2e502e2d0aa2b] so export will work with html entities and special chars.

--- src/controllers/setup/setup_exportapplication.php
+++ src/controllers/setup/setup_exportapplication.php
@@ -10,9 +10,9 @@ class SetupExportApplicationController extends \Jazzee\AdminController {
   const MENU = 'Setup';
   const TITLE = 'Save Configuration';
   const PATH = 'setup/exportapplication';
-  
+
   const ACTION_INDEX = 'Export Configuration';
-  
+
   /**
    * If there is no application then create a new one to work with
    */
@@ -23,7 +23,7 @@ class SetupExportApplicationController extends \Jazzee\AdminController {
       $this->redirectPath('welcome');
     }
   }
-  
+
   /**
    * Setup the current application and cycle
    */
@@ -32,10 +32,10 @@ class SetupExportApplicationController extends \Jazzee\AdminController {
     $this->setLayoutVar('filename', $this->_application->getProgram()->getShortName() . '-' . $this->_application->getCycle()->getName() . '.xml');
     $this->setVar('application', $this->_application);
   }
-  
+
   /**
    * Create an xml node for a page
-   * 
+   *
    * Calls itself recursivly to capture all children
    * @param DomDocument $xml
    * @param \Jazzee\Entity\Page $page
@@ -47,15 +47,11 @@ class SetupExportApplicationController extends \Jazzee\AdminController {
     $pxml->setAttribute('max', $page->getMax());
     $pxml->setAttribute('required', $page->isRequired());
     $pxml->setAttribute('answerStatusDisplay', $page->answerStatusDisplay());
-    $element = $dom->createElement('instructions');
-    if($page->getInstructions()) $element->appendChild($dom->createCDATASection($page->getInstructions()));
-    $pxml->appendChild($element);
-    $element = $dom->createElement('leadingText');
-    if($page->getLeadingText()) $element->appendChild($dom->createCDATASection($page->getLeadingText()));
-    $pxml->appendChild($element);
-    $element = $dom->createElement('trailingText');
-    if($page->getTrailingText()) $element->appendChild($dom->createCDATASection($page->getTrailingText()));
-    $pxml->appendChild($element);
+    
+    $pxml->appendChild($this->createCdataElement($dom, 'instructions', $page->getInstructions()));
+    $pxml->appendChild($this->createCdataElement($dom, 'leadingText', $page->getLeadingText()));
+    $pxml->appendChild($this->createCdataElement($dom, 'trailingText', $page->getTrailingText()));
+
     if($page instanceof \Jazzee\Entity\ApplicationPage){
       $pxml->setAttribute('weight', $page->getWeight());
       $pxml->setAttribute('kind', $page->getKind());
@@ -66,7 +62,7 @@ class SetupExportApplicationController extends \Jazzee\AdminController {
       }
     }
     $pxml->setAttribute('class', $page->getType()->getClass());
-    
+
     $elements = $pxml->appendChild($dom->createElement('elements'));
     foreach($page->getElements() as $element){
       $exml = $dom->createElement('element');
@@ -96,14 +92,29 @@ class SetupExportApplicationController extends \Jazzee\AdminController {
     }
     $children = $pxml->appendChild($dom->createElement('children'));
     foreach($page->getChildren() as $child) $children->appendChild($this->pageXml($dom, $child));
-    
+
     $variables = $pxml->appendChild($dom->createElement('variables'));
     foreach($page->getVariables() as $var){
       $variable = $dom->createElement('variable', (string)$var->getValue());
       $variable->setAttribute('name', $var->getName());
       $variables->appendChild($variable);
-    } 
+    }
     return $pxml;
   }
-  
+
+  /**
+   * Create a CDATA section from a value
+   * @param DOMDocument $dom
+   * @param string $elementName
+   * @param string $value
+   * @return \DOMElement
+   */
+  public function createCdataElement(\DOMDocument $dom, $elementName, $value){
+    $element = $dom->createElement($elementName);
+    if($value){
+      $element->appendChild($dom->createCDATASection($value));
+    }
+    return $element;
+  }
+
 }
